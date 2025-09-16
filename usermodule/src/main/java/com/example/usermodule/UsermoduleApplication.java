3b04b121b430daaccacc77671e736ac2790a81b1
Dinh Dat <tddat19102004@gmail.com>
2025-09-17 01:09:04 +0700

    d

--- usermodule/src/main/java/com/example/usermodule/UsermoduleApplication.java
+++ usermodule/src/main/java/com/example/usermodule/UsermoduleApplication.java
@@ -1,5 +1,6 @@
 package com.example.usermodule;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UsermoduleApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load(); // load .env
+		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
 		SpringApplication.run(UsermoduleApplication.class, args);
 	}
 
